ed389f0a890fbafbc9bd8e2fc83a00dba557b53e
Gabriel <bielbobrov@hotmail.com>
2021-02-18 10:21:43 -0300

    Forum

---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,55 @@
+package br.com.alura.forum.controller.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+
+
+public class TopicoForm {
+	
+	@NotNull
+	@NotEmpty
+	@Length(min = 10, message = "O titulo deve ter 10 caracteres")
+	private String titulo;
+	
+	@NotNull
+	@NotEmpty
+	@Length(min = 10, message = "A mensagem deve ter 10 caracteres")
+	private String mensagem;
+	
+	@NotNull
+	@NotEmpty
+	@Length(min = 10, message = "O nome do curso deve ter 10 caracteres")
+	private String nomeCurso;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	public Topico converter(CursoRepository cursoRepository) {
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		return new Topico(titulo, mensagem, curso);
+	}
+	
+	
+
+}
